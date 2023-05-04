bd43493eb4f97ad6238a3daa806d1d07a5aff383
nelsonlucianoembana <129955553+nelsonlucianoembana@users.noreply.github.com>
2023-05-04 12:25:22 +0100

    Atualizado

---
+++ ex017/script.js
@@ -0,0 +1,14 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6) 
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}  É a mesma coisa com o de cima
+*/
---
+++ ex018/script.js
@@ -0,0 +1,11 @@
+/*var c = 1
+while (c <= 10) {
+    console.log
+    c++
+}   É a mesma coisa com o de baixo*/
+
+//--------------------
+
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
