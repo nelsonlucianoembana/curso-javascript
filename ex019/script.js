4df21468b2a0a5df5be08d6fc1541582392e30ab
nelsonlucianoembana <129955553+nelsonlucianoembana@users.noreply.github.com>
2023-05-08 16:19:16 +0100

    Atualizado

--- ex019/script.js
+++ ex019/script.js
@@ -6,6 +6,23 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
         alert("[ERRO] Complete o campo para poder contar!")
     } else {
-       res.innerHTML = "Contando..."
+       res.innerHTML = "Contando:"
+       let i = Number(ini.value)
+       let f = Number(fim.value)
+       let p = Number(pas.value)
+       if (i < f) {
+        //contagem crescente
+        for (let c = 1; c <= f; c += p ) {
+        res.innerHTML += ` ${c} ` 
+       }
+
+       } else {
+         //contagem regressiva
+        for (let c = 1; c >= f; c -= p){
+         res.innerHTML += ` ${c}`
+        }
+      
+       }
+        res.innerHTML += "<strong>Fim</strong>"
     }
 }
